9c6c29639be7d087ca751dc8d0d8877f553bfe8e
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-08-18 17:24:47 +0100

    changes made as UthandoUser acl changed

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,7 @@ return [
 	'theme_manager' => [
         'default_theme'     => 'default',
         'admin_theme'       => 'admin',
-        'theme_path'        => APPLICATION_PATH . '/public/themes/',
+        'theme_path'        => './public/themes/',
         'bootstrap'         => true,
         'bootswatch_theme'  => null,
         'font_awesome'      => true,
