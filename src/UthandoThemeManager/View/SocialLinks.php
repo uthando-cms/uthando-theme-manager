9c27158c156d4299e108f298b25bc438e294daf4
shaunfreeman <shaun@shaunfreeman.co.uk>
2017-03-04 13:20:14 +0000

    added custom class for helper

--- src/UthandoThemeManager/View/SocialLinks.php
+++ src/UthandoThemeManager/View/SocialLinks.php
@@ -21,9 +21,22 @@ use UthandoCommon\View\AbstractViewHelper;
  */
 class SocialLinks extends AbstractViewHelper
 {
-    public function __invoke()
+    /**
+     * @var string
+     */
+    protected $class = 'social fa fa-';
+
+    /**
+     * @param null|string $class
+     * @return string
+     */
+    public function __invoke($class = null)
     {
         $config = $this->getConfig('theme_manager');
+
+        if ($class) {
+            $this->setClass($class);
+        }
 
         if (isset($config['social_links']) && is_array($config['social_links'])) {
             return $this->render($config['social_links']);
@@ -32,6 +45,10 @@ class SocialLinks extends AbstractViewHelper
         return '';
     }
 
+    /**
+     * @param array $links
+     * @return string
+     */
     public function render(array $links)
     {
         $html = "";
@@ -45,5 +62,21 @@ class SocialLinks extends AbstractViewHelper
         }
 
         return $html;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * @param string $class
+     */
+    public function setClass($class)
+    {
+        $this->class = $class;
     }
 } 
